e75c4d5b30a585d1f866e1aef564c8cd8494fcdc
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2021-12-02 12:18:35 -0800

    set up basic rotating diamond

--- main.js
+++ main.js
@@ -1,2 +1,37 @@
 import './style.css'
 
+import * as THREE from 'three';
+
+
+//set up scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg")
+})
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(10);
+
+
+//define elements
+const geometry = new THREE.OctahedronGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0xff00ff} );
+const diamond = new THREE.Mesh( geometry, material);
+
+//add elements to scene
+scene.add( diamond );
+
+
+//animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  
+  diamond.rotation.x += 0.01;
+  diamond.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
